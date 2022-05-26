791295360f6d9a92243f98a2f12cda3eb5b520ac
smartoctopus <alessandro.bozzato.2007@outlook.it>
2022-05-26 22:32:22 +0200

    Update example

--- examples/main.c
+++ examples/main.c
@@ -1,16 +1,22 @@
-#include "test.h"
+#include "../test.h"
 
 test("Testing test") {
-  before_each() { printf("Hello\n"); }
-  after_each() { printf("World\n"); }
-  before() { printf("Ciao mondo\n"); }
-  after() { printf("Hello world\n"); }
+  int val = 1;
+  before_each() { val = 0; }
+  after_each() { val = 1; }
+  /* before() { printf("Ciao mondo\n"); } */
+  /* after() { printf("Hello world\n"); } */
+
+  it("Runs") { expect(4 == 3); }
+  it("Runs another time") { expect(val == 0); }
   describe("Hello") {
-    it("Says Hello") { expect(strcmp("Hello", "Bye") == 0); }
+    it("Says Hello") { expect(strcmp("Hello", "Bye") != 0); }
   }
-  skip("Runs fine") { expect(1 == 0); }
-  it("Runs0") { expect(0 == 4); }
-  it("Runs1") { expect(1 == 4); }
-  it("Runs2") { expect(2 == 4); }
-  it("Runs3") { expect(3 == 4); }
+  describe("two") {
+    skip("Runs fine") { expect(1 == 0); }
+    it("Runs0") { expect(4 == 2); }
+    it("Runs1") { expect(4 == 4); }
+    it("Runs2") { expect(4 == 3); }
+    it("Runs3") { expect(4 == 4); }
+  }
 }

7bb7acec37facb45e16f56180c58e1f833f1509b
smartoctopus <alessandro.bozzato.2007@outlook.it>
2022-05-27 22:01:41 +0200

    Applied new formatting

--- examples/main.c
+++ examples/main.c
@@ -1,22 +1,25 @@
 #include "../test.h"
 
-test("Testing test") {
-  int val = 1;
-  before_each() { val = 0; }
-  after_each() { val = 1; }
-  /* before() { printf("Ciao mondo\n"); } */
-  /* after() { printf("Hello world\n"); } */
+test("Testing test")
+{
+    int val = 1;
+    before_each() { val = 0; }
+    after_each() { val = 1; }
+    /* before() { printf("Ciao mondo\n"); } */
+    /* after() { printf("Hello world\n"); } */
 
-  it("Runs") { expect(4 == 3); }
-  it("Runs another time") { expect(val == 0); }
-  describe("Hello") {
-    it("Says Hello") { expect(strcmp("Hello", "Bye") != 0); }
-  }
-  describe("two") {
-    skip("Runs fine") { expect(1 == 0); }
-    it("Runs0") { expect(4 == 2); }
-    it("Runs1") { expect(4 == 4); }
-    it("Runs2") { expect(4 == 3); }
-    it("Runs3") { expect(4 == 4); }
-  }
+    it("Runs") { expect(4 == 3); }
+    it("Runs another time") { expect(val == 0); }
+    describe("Hello")
+    {
+        it("Says Hello") { expect(strcmp("Hello", "Bye") != 0); }
+    }
+    describe("two")
+    {
+        skip("Runs fine") { expect(1 == 0); }
+        it("Runs0") { expect(4 == 2); }
+        it("Runs1") { expect(4 == 4); }
+        it("Runs2") { expect(4 == 3); }
+        it("Runs3") { expect(4 == 4); }
+    }
 }
--- test.h
+++ test.h
@@ -18,46 +18,46 @@
 #define COLOR_WHITE "\033[1;37m"
 
 /* Fundamental macros */
-#define test(_str)                                                             \
-  const char *test_name = (_str);                                              \
-  void test_func(TestContext *ctx)
+#define test(_str)                  \
+    const char* test_name = (_str); \
+    void test_func(TestContext* ctx)
 
-#define describe__(_str, _ident)                                               \
-  for (int prev = ctx->current_describe,                                       \
-           _ident = register_describe(ctx, _str);                              \
-       _ident && run_describe(ctx, _str); _ident = describe_end(ctx, prev))
+#define describe__(_str, _ident)                    \
+    for (int prev = ctx->current_describe,          \
+             _ident = register_describe(ctx, _str); \
+         _ident && run_describe(ctx, _str); _ident = describe_end(ctx, prev))
 #define describe_(_str, _ident) describe__(_str, _ident)
 #define describe(_str) describe_((_str), RANDOM_IDENT(describe))
 
-#define it__(_str, _ident)                                                     \
-  for (bool _ident = register_it(ctx, _str, false);                            \
-       _ident && run_it(ctx, _str); _ident = false)
+#define it__(_str, _ident)                            \
+    for (bool _ident = register_it(ctx, _str, false); \
+         _ident && run_it(ctx, _str); _ident = false)
 #define it_(_str, _ident) it__(_str, _ident)
 #define it(_str) it_((_str), RANDOM_IDENT(it))
 
 #define skip(_str) for ((void)register_it(ctx, (_str), true); false;)
 
-#define before__(_ident)                                                       \
-  for (bool _ident = register_before(ctx); _ident && run_it(ctx, "before");    \
-       _ident = false)
+#define before__(_ident)                                                      \
+    for (bool _ident = register_before(ctx); _ident && run_it(ctx, "before"); \
+         _ident = false)
 #define before_(_ident) before__(_ident)
 #define before() before_(RANDOM_IDENT(before))
 
-#define before_each__(_ident)                                                  \
-  for (bool _ident = register_before_each(ctx);                                \
-       _ident && run_it(ctx, "before_each"); _ident = false)
+#define before_each__(_ident)                     \
+    for (bool _ident = register_before_each(ctx); \
+         _ident && run_it(ctx, "before_each"); _ident = false)
 #define before_each_(_ident) before_each__(_ident)
 #define before_each() before_each_(RANDOM_IDENT(before_each))
 
-#define after__(_ident)                                                        \
-  for (bool _ident = register_after(ctx); _ident && run_it(ctx, "after");      \
-       _ident = false)
+#define after__(_ident)                                                     \
+    for (bool _ident = register_after(ctx); _ident && run_it(ctx, "after"); \
+         _ident = false)
 #define after_(_ident) after__(_ident)
 #define after() after_(RANDOM_IDENT(after))
 
-#define after_each__(_ident)                                                   \
-  for (bool _ident = register_after_each(ctx);                                 \
-       _ident && run_it(ctx, "after_each"); _ident = false)
+#define after_each__(_ident)                     \
+    for (bool _ident = register_after_each(ctx); \
+         _ident && run_it(ctx, "after_each"); _ident = false)
 #define after_each_(_ident) after_each__(_ident)
 #define after_each() after_each_(RANDOM_IDENT(after_each))
 
@@ -65,225 +65,232 @@
 
 /* Types */
 typedef struct Test {
-  const char *name;
-  bool skip;
+    const char* name;
+    bool skip;
 } Test;
 
 typedef struct Describe {
-  uint32_t current_test;
-  int8_t before;
-  int8_t after;
-  int8_t before_each;
-  int8_t after_each;
-  const char *name;
-  array(Test) tests;
+    uint32_t current_test;
+    int8_t before;
+    int8_t after;
+    int8_t before_each;
+    int8_t after_each;
+    const char* name;
+    array(Test) tests;
 } Describe;
 
 typedef struct TestContext {
-  bool use_color;
-  bool pre_run;
-  uint16_t passed_tests;
-  uint16_t skipped_tests;
-  uint16_t failed_tests;
-  uint32_t current_describe;
-  uint32_t indent;
-  array(Describe) describes;
+    bool use_color;
+    bool pre_run;
+    uint16_t passed_tests;
+    uint16_t skipped_tests;
+    uint16_t failed_tests;
+    uint32_t current_describe;
+    uint32_t indent;
+    array(Describe) describes;
 } TestContext;
 
 /* Functions */
-bool cmp_str(const char *s1, const char *s2) {
-  size_t s1_length = strlen(s1);
-  size_t s2_length = strlen(s2);
-  if (s1_length != s2_length) {
-    return false;
-  }
-  return strncmp(s1, s2, s1_length) == 0;
+bool cmp_str(const char* s1, const char* s2)
+{
+    size_t s1_length = strlen(s1);
+    size_t s2_length = strlen(s2);
+    if (s1_length != s2_length) {
+        return false;
+    }
+    return strncmp(s1, s2, s1_length) == 0;
 }
 
-int register_describe(TestContext *ctx, const char *name) {
-  if (ctx->pre_run) {
-    Describe describe;
-    describe.name = name;
-    describe.tests = NULL;
-    describe.current_test = 0;
-    describe.before = -1;
-    describe.after = -1;
-    describe.before_each = -1;
-    describe.after_each = -1;
+int register_describe(TestContext* ctx, const char* name)
+{
+    if (ctx->pre_run) {
+        Describe describe;
+        describe.name = name;
+        describe.tests = NULL;
+        describe.current_test = 0;
+        describe.before = -1;
+        describe.after = -1;
+        describe.before_each = -1;
+        describe.after_each = -1;
 
-    array_push(ctx->describes, describe);
-    ctx->current_describe = array_length(ctx->describes) - 1;
-  }
-  return true;
-}
-
-bool run_describe(TestContext *ctx, const char *name) {
-  if (ctx->pre_run) {
+        array_push(ctx->describes, describe);
+        ctx->current_describe = array_length(ctx->describes) - 1;
+    }
     return true;
-  }
-  return cmp_str(ctx->describes[ctx->current_describe].name, name);
 }
 
-int describe_end(TestContext *ctx, int prev) {
-  ctx->current_describe = prev;
-  return false;
+bool run_describe(TestContext* ctx, const char* name)
+{
+    if (ctx->pre_run) {
+        return true;
+    }
+    return cmp_str(ctx->describes[ctx->current_describe].name, name);
 }
 
-bool register_it(TestContext *ctx, const char *name, bool skip) {
-  if (ctx->pre_run) {
-    Test test;
-    test.name = name;
-    test.skip = skip;
-    array_push(ctx->describes[ctx->current_describe].tests, test);
-  }
-  return true;
-}
-
-bool run_it(TestContext *ctx, const char *name) {
-  if (ctx->pre_run) {
+int describe_end(TestContext* ctx, int prev)
+{
+    ctx->current_describe = prev;
     return false;
-  }
-  Describe describe = ctx->describes[ctx->current_describe];
-  return cmp_str(describe.tests[describe.current_test].name, name);
 }
 
-bool register_before(TestContext *ctx) {
-  if (ctx->pre_run) {
-    Test before = {"before", false};
-    array_push(ctx->describes[ctx->current_describe].tests, before);
-    ctx->describes[ctx->current_describe].before =
-        array_length(ctx->describes[ctx->current_describe].tests) - 1;
-  }
-  return true;
+bool register_it(TestContext* ctx, const char* name, bool skip)
+{
+    if (ctx->pre_run) {
+        Test test;
+        test.name = name;
+        test.skip = skip;
+        array_push(ctx->describes[ctx->current_describe].tests, test);
+    }
+    return true;
 }
 
-bool register_before_each(TestContext *ctx) {
-  if (ctx->pre_run) {
-    Test before_each = {"before_each", false};
-    array_push(ctx->describes[ctx->current_describe].tests, before_each);
-    ctx->describes[ctx->current_describe].before_each =
-        array_length(ctx->describes[ctx->current_describe].tests) - 1;
-  }
-  return true;
+bool run_it(TestContext* ctx, const char* name)
+{
+    if (ctx->pre_run) {
+        return false;
+    }
+    Describe describe = ctx->describes[ctx->current_describe];
+    return cmp_str(describe.tests[describe.current_test].name, name);
 }
 
-bool register_after(TestContext *ctx) {
-  if (ctx->pre_run) {
-    Test after = {"after", false};
-    array_push(ctx->describes[ctx->current_describe].tests, after);
-    ctx->describes[ctx->current_describe].after =
-        array_length(ctx->describes[ctx->current_describe].tests) - 1;
-  }
-  return true;
+bool register_before(TestContext* ctx)
+{
+    if (ctx->pre_run) {
+        Test before = { "before", false };
+        array_push(ctx->describes[ctx->current_describe].tests, before);
+        ctx->describes[ctx->current_describe].before = array_length(ctx->describes[ctx->current_describe].tests) - 1;
+    }
+    return true;
 }
 
-bool register_after_each(TestContext *ctx) {
-  if (ctx->pre_run) {
-    Test after_each = {"after_each", false};
-    array_push(ctx->describes[ctx->current_describe].tests, after_each);
-    ctx->describes[ctx->current_describe].after_each =
-        array_length(ctx->describes[ctx->current_describe].tests) - 1;
-  }
-  return true;
+bool register_before_each(TestContext* ctx)
+{
+    if (ctx->pre_run) {
+        Test before_each = { "before_each", false };
+        array_push(ctx->describes[ctx->current_describe].tests, before_each);
+        ctx->describes[ctx->current_describe].before_each = array_length(ctx->describes[ctx->current_describe].tests) - 1;
+    }
+    return true;
 }
 
-void expect_(TestContext *ctx, bool value, const char *expr) {
-  if (value) {
-    ctx->passed_tests++;
-    fprintf(stderr, "%*s" COLOR_GREEN "✓" COLOR_RESET " %s\n", ctx->indent * 2,
+bool register_after(TestContext* ctx)
+{
+    if (ctx->pre_run) {
+        Test after = { "after", false };
+        array_push(ctx->describes[ctx->current_describe].tests, after);
+        ctx->describes[ctx->current_describe].after = array_length(ctx->describes[ctx->current_describe].tests) - 1;
+    }
+    return true;
+}
+
+bool register_after_each(TestContext* ctx)
+{
+    if (ctx->pre_run) {
+        Test after_each = { "after_each", false };
+        array_push(ctx->describes[ctx->current_describe].tests, after_each);
+        ctx->describes[ctx->current_describe].after_each = array_length(ctx->describes[ctx->current_describe].tests) - 1;
+    }
+    return true;
+}
+
+void expect_(TestContext* ctx, bool value, const char* expr)
+{
+    if (value) {
+        ctx->passed_tests++;
+        fprintf(stderr, "%*s" COLOR_GREEN "✓" COLOR_RESET " %s\n", ctx->indent * 2,
             "", expr);
-  } else {
-    ctx->failed_tests++;
-    fprintf(stderr, "%*s" COLOR_RED "◯" COLOR_RESET " %s\n", ctx->indent * 2,
+    } else {
+        ctx->failed_tests++;
+        fprintf(stderr, "%*s" COLOR_RED "◯" COLOR_RESET " %s\n", ctx->indent * 2,
             "", expr);
-  }
+    }
 }
 
 #ifndef CUSTOM_MAIN
-extern const char *test_name;
-extern void test_func(TestContext *ctx);
+extern const char* test_name;
+extern void test_func(TestContext* ctx);
 
-int main(void) {
-  TestContext ctx = {0};
+int main(void)
+{
+    TestContext ctx = { 0 };
 
-  /* Pre run */
-  ctx.pre_run = true;
-  (void)register_describe(&ctx, test_name);
-  test_func(&ctx);
+    /* Pre run */
+    ctx.pre_run = true;
+    (void)register_describe(&ctx, test_name);
+    test_func(&ctx);
 
-  /* Actual run */
-  ctx.pre_run = false;
-  for (; ctx.current_describe < array_length(ctx.describes);
-       ctx.current_describe++) {
-    Describe *describe = &ctx.describes[ctx.current_describe];
-    fprintf(stderr, "%*s• %s:\n", ctx.indent * 2, "", describe->name);
-    // Describe indentation
-    ctx.indent++;
-    /* Before */
-    if (describe->before != -1) {
-      describe->current_test = describe->before;
-      test_func(&ctx);
-      describe->current_test = 0;
+    /* Actual run */
+    ctx.pre_run = false;
+    for (; ctx.current_describe < array_length(ctx.describes);
+         ctx.current_describe++) {
+        Describe* describe = &ctx.describes[ctx.current_describe];
+        fprintf(stderr, "%*s• %s:\n", ctx.indent * 2, "", describe->name);
+        // Describe indentation
+        ctx.indent++;
+        /* Before */
+        if (describe->before != -1) {
+            describe->current_test = describe->before;
+            test_func(&ctx);
+            describe->current_test = 0;
+        }
+        while (describe->current_test < array_length(describe->tests)) {
+            int test = describe->current_test;
+            if (test == describe->before || test == describe->before_each || test == describe->after || test == describe->after_each) {
+                describe->current_test++;
+                continue;
+            }
+
+            if (describe->before_each != -1) {
+                describe->current_test = describe->before_each;
+                test_func(&ctx);
+                describe->current_test = test;
+            }
+
+            fprintf(stderr, "%*s• %s:", ctx.indent * 2, "",
+                describe->tests[test].name);
+            if (describe->tests[test].skip) {
+                ctx.skipped_tests++;
+                fprintf(stderr, " " COLOR_YELLOW "Skipped" COLOR_RESET);
+            }
+            fprintf(stderr, "\n");
+
+            // Test
+            ctx.indent++;
+            test_func(&ctx);
+            ctx.indent--;
+
+            if (describe->after_each != -1) {
+                describe->current_test = describe->after_each;
+                test_func(&ctx);
+                describe->current_test = test;
+            }
+
+            describe->current_test++;
+        }
+        if (describe->after != -1) {
+            describe->current_test = describe->after;
+            test_func(&ctx);
+        }
+
+        if (cmp_str(describe->name, test_name)) {
+            ctx.indent++;
+        }
+
+        // Describe indentation
+        ctx.indent--;
     }
-    while (describe->current_test < array_length(describe->tests)) {
-      int test = describe->current_test;
-      if (test == describe->before || test == describe->before_each ||
-          test == describe->after || test == describe->after_each) {
-        describe->current_test++;
-        continue;
-      }
 
-      if (describe->before_each != -1) {
-        describe->current_test = describe->before_each;
-        test_func(&ctx);
-        describe->current_test = test;
-      }
-
-      fprintf(stderr, "%*s• %s:", ctx.indent * 2, "",
-              describe->tests[test].name);
-      if (describe->tests[test].skip) {
-        ctx.skipped_tests++;
-        fprintf(stderr, " " COLOR_YELLOW "Skipped" COLOR_RESET);
-      }
-      fprintf(stderr, "\n");
-
-      // Test
-      ctx.indent++;
-      test_func(&ctx);
-      ctx.indent--;
-
-      if (describe->after_each != -1) {
-        describe->current_test = describe->after_each;
-        test_func(&ctx);
-        describe->current_test = test;
-      }
-
-      describe->current_test++;
-    }
-    if (describe->after != -1) {
-      describe->current_test = describe->after;
-      test_func(&ctx);
-    }
-
-    if (cmp_str(describe->name, test_name)) {
-      ctx.indent++;
-    }
-
-    // Describe indentation
+    // Print the report
     ctx.indent--;
-  }
+    fprintf(stderr, "\n" COLOR_WHITE "Tests:" COLOR_RESET "\n");
+    fprintf(stderr, "  • " COLOR_GREEN "%d Passed" COLOR_RESET "\n",
+        ctx.passed_tests);
+    fprintf(stderr, "  • " COLOR_YELLOW "%d Skipped" COLOR_RESET "\n",
+        ctx.skipped_tests);
+    fprintf(stderr, "  • " COLOR_RED "%d Failed" COLOR_RESET "\n",
+        ctx.failed_tests);
 
-  // Print the report
-  ctx.indent--;
-  fprintf(stderr, "\n" COLOR_WHITE "Tests:" COLOR_RESET "\n");
-  fprintf(stderr, "  • " COLOR_GREEN "%d Passed" COLOR_RESET "\n",
-          ctx.passed_tests);
-  fprintf(stderr, "  • " COLOR_YELLOW "%d Skipped" COLOR_RESET "\n",
-          ctx.skipped_tests);
-  fprintf(stderr, "  • " COLOR_RED "%d Failed" COLOR_RESET "\n",
-          ctx.failed_tests);
-
-  return 0;
+    return 0;
 }
 #endif
 
